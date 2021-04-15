e7adbe54ed06ce6ae5f663b1c98053d710994256
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-15 11:24:34 +0200

    Main frame added

---
+++ src/components/MainFrame.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div id="mainTopBar">
+            <div id="top"></div>
+            <div id="bottom">
+                <div id="titleBar"><div id="titleBar-inside">{{title}}</div></div>
+                <buttonMainFrame></buttonMainFrame>
+                <div style="clear: both;"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+import buttonMainFrame from './buttonMainFrame.vue'
+export default {
+  components: { buttonMainFrame },
+    data(){
+        return{
+            title: 'title'
+        }
+    }
+}
+</script>
+
+
+<style>
+    #mainTopBar{
+        width: 100%;
+        height: 100px;
+        border-bottom: solid 3px gray;
+    }
+
+    #top{
+        height: 50px;
+        width: 100%;
+    }
+
+    #bottom{
+        height: 50px;
+        width: 100%;
+    }
+
+    #titleBar{
+        background-color: lightgray;
+        width: 300px;
+        height: 48px;
+        border: gray solid 2px;
+        border-top-left-radius: 10px;
+        border-top-right-radius: 10px;
+        float: left;
+    }
+
+    #titleBar-inside{
+        padding-left: 20px;
+        padding-top: 5px;
+        font-size: 30px;
+    }
+</style>
