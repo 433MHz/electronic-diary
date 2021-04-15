85af3a0657c6627d40e7b2ad7512e6a2346b0d15
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-15 11:28:19 +0200

    Button completed

---
+++ src/components/buttonMainFrame.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="main">
+        <button id="buttonEdit">ghgh</button>
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+}
+</script>
+
+
+<style>
+    #main{
+        width: 100px;
+        height: 50px;
+        float: left;
+        text-align: center;
+    }
+
+    #buttonEdit{
+        width: 95px;
+        height: 30px;
+        font-size: 15px;
+        margin-top: 18px;
+    }
+
+    #buttonEdit:hover{
+        cursor: pointer;
+    }
+
+</style>
