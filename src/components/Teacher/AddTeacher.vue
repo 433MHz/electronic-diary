14b075e04f471706a173342d4360db3a5bb281c4
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-16 02:50:39 +0200

    Added components to add and to show teacher

---
+++ src/components/Teacher/AddTeacher.vue
@@ -0,0 +1,28 @@
+<template>
+    <div id="AddTeacherContainer">
+        <input type="text" id="firstName" placeholder="Imie"><br>
+        <input type="text" id="secondName" placeholder="Nazwisko"><br>
+        <button-main-frame title="Dodaj" @click="PerformButton()"></button-main-frame>
+    </div>
+</template>
+
+
+<script>
+import buttonMainFrame from '../buttonMainFrame.vue'
+export default {
+  components: { buttonMainFrame },
+    
+    methods:{
+        PerformButton(){
+            alert("do something")
+        }
+    }
+}
+</script>
+
+
+<style>
+    #AddTeacherContainer input{
+        margin-top: 10px;
+    }
+</style>
