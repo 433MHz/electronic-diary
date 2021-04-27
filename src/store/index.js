28071f004f2a6dbdee0fa2c2ce0498064fec44c0
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-27 17:30:31 +0200

    Added data fetch api's

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,57 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
-    title: 'Dziennik elektroniczny'
+    title: 'Dziennik elektroniczny',
+
+    teachers: [],
+    classes: [],
+    students: [],
+    subjects: []
   },
 
   mutations: {
     setTitle (state, payload){
-      state.title = payload}
+      state.title = payload
+    },
+
+    setTeachers (state, payload){
+      state.teachers = payload
+    },
+
+    setClasses(state, payload){
+      state.classes = payload
+    },
+
+    setStudents(state,payload){
+      state.students = payload
+    },
+
+    setSubjects(state, payload){
+      state.subjects = payload
+    }
   },
 
   getters: {
-    getTitle: state => {return state.title}
+    getTitle: state => {return state.title},
+
+    getTeacher: state => {return state.teachers}
+  },
+
+  actions: {
+    updateTeachers(){
+      fetch('http://localhost:8081/teachers/getAll').then(res => res.json()).then(data => this.commit('setTeachers', data))
+    },
+    
+    updateClasses(){
+      fetch('http://localhost:8081/classes/getAll').then(res => res.json()).then(data => this.commit('setClasses', data))
+    },
+
+    updateStudents(){
+      fetch('http://localhost:8081/students/getAll').then(res => res.json()).then(data => this.commit('setStudents', data))
+    },
+
+    updateSubjects(){
+      fetch('http://localhost:8081/subjects/getAll').then(res => res.json()).then(data => this.commit('setSubjects', data))
+    }
   }
 })
