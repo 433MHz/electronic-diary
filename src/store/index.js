16cb62b0387c00d37831eb2266762027bc2f4160
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-04-27 13:30:26 +0200

    Maked title variable as globally accessible to change it dynamically

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,15 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    title: 'Dziennik elektroniczny'
   },
+
   mutations: {
+    setTitle (state, payload){
+      state.title = payload}
   },
-  actions: {
-  },
-  modules: {
+
+  getters: {
+    getTitle: state => {return state.title}
   }
 })
